b191d22af68757adc86fe01aa5e3e7e2c0a16549
Awawa <69086569+awawa-dev@users.noreply.github.com>
2026-03-04 21:14:55 +0100

    Fix ETH_PHY_TYPE detection

--- src/leds.cpp
+++ src/leds.cpp
@@ -55,7 +55,7 @@
         typedef NeoPixelBus<NeoGrbFeature, NeoEsp32I2s0Ws2812xMethod> NeoPixel;
         typedef NeoPixelBus<NeoGrbwFeature, NeoEsp32I2s0Sk6812Method> NeoPixelRgbw;
     #elif defined(ARDUINO_ARCH_ESP32)
-        #if ETH_PHY_TYPE == ETH_PHY_LAN8720
+        #if defined(ETH_PHY_TYPE) && (ETH_PHY_TYPE == ETH_PHY_LAN8720)
             typedef NeoPixelBus<DotStarBgrFeature, DotStarMethod> DotStar;
         #else
             typedef NeoPixelBus<DotStarBgrFeature, DotStarSpi10MhzMethod > DotStar;
